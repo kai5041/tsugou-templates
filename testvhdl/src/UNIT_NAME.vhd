62173cf9fb36d382597244b92729362dabf04312
kai5041 <kai5041contact@gmail.com>
2025-01-05 22:45:30 +0100

    Backup, experimenting cmake + vhdl

---
+++ testvhdl/src/UNIT_NAME.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity UNIT_NAME is
+    --port (
+    --  x : in std_logic;
+    --  y : in std_logic;
+    --);
+end entity UNIT_NAME;
+
+architecture UNIT_NAME_behavior of UNIT_NAME is
+begin
+end architecture UNIT_NAME_behavior;
